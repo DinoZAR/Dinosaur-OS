17786939666e231558852fdff330e555d162e4a3
Spencer Graffe <sgraffe@gmail.com>
2011-10-26 14:53:09 -0700

    Wrote some BIOS string printing functions. Starting on disk reading later.

--- Dinosaur/bootloader/bootloader.asm
+++ Dinosaur/bootloader/bootloader.asm
@@ -22,13 +22,57 @@ start:
 	mov dh, 0
 	int 0x10
 	
-	; Teletype a single character to the screen
-	mov ah, 0x0E
-	mov al, "L"
-	mov bl, 0x04
-	int 0x10
+	; Write "Welcome to Dinosaur!" to the screen
+	mov si, welcome_string		; Copy address of welcome_string to si
+	call PrintString
+	
+	call PrintHorizRule
+	
+	mov si, mesg_hdd_contents
+	call PrintString
 	
 	hlt
+	
+	
+PrintString:
+	mov ah, 0x0E		; Teletype function
+	cld
+	
+	PrintString_begin:
+		lodsb
+		or al, al
+		jz PrintString_end
+		int 0x10
+		jmp PrintString_begin
+		
+	PrintString_end:
+		ret
+
+PrintHorizRule:
+	mov ah, 0x0E		; Teletype function
+	mov al, "_"
+	mov cx, 80
+	
+	PrintHorizRule_begin:
+		dec cx
+		jz PrintHorizRule_end
+		int 0x10
+		jmp PrintHorizRule_begin
+		
+	PrintHorizRule_end:
+		mov al, CR			; Add trailing CR and LF
+		int 0x10
+		mov al, LF
+		int 0x10
+		ret
+	
+data:
+	%define CR 13
+	%define LF 10
+	
+	welcome_string db "Welcome to Dinosaur!",CR,LF,0
+	
+	mesg_hdd_contents db "Reading 2nd Stage image...",0
 	
 end_filler:
     times 510 - ($ - $$) db 0		; Fills rest of sector with 0's
