195a9fb7cd454ba1097931497e2f9b10046b9514
Spencer Graffe <sgraffe@gmail.com>
2011-11-08 20:14:51 -0800

    Started work on 'new' command for RAWR. Didn't do much beyond that.

--- Dinosaur/filesystem-rawr/rawr.py
+++ Dinosaur/filesystem-rawr/rawr.py
@@ -1,10 +1,13 @@
 # This is the entry point from which scripts needing to do operations with the
-# filesystem should go to. It can create new ones, write files to it, back it
+# file system should go to. It can create new ones, write files to it, back it
 # up, and repair it if needed be. It does all of its operations on an image,
 # which does not include an MBR nor a partition table.
 #
 # The file system image is stored outside of the working directory and is saved 
-# as Dinosaur_RAWR.img
+# as Dinosaur_RAWR.img.
+#
+# This program only creates RAWR file systems, or Ridiculously Awesome Writer
+# Reader file systems.
 
 import sys
 import os
@@ -25,20 +28,32 @@ def properUse():
 	
 	# Backup command
 	pString += 'backup - Backs up your filesystem and saves it\n'
-	pString += '-o dest: Saves backup to destination'
+	pString += '-o dest: Saves backup to destination\n\n'
+	
+	# Write command
+	pString += 'write - Writes a file to the filesystem\n'
+	pString += '-w: Warn if this file already exists\n'
+	pString += '-a: Append rather than overwrite an existing file\n'
+	pString += '[Note: Files to be written must use Unix absolute paths]'
+	
 	return pString
 	
 
 def new():
-	pass
 	
-def backup():
+	# Make a brand-new RAWR file system
+	outFileName = os.path.abspath('../../../Dinosaur_RAWR.img')
+	
+	outFile = open(outFileName, 'wb')
+	
+	
+	
+	outFile.close()
+	
+def backup(outputFile):
 	pass
 
-def check():
-	pass
-
-def write():
+def write(file, warning, append):
 	pass
 
 	
@@ -47,14 +62,48 @@ def write():
 # ------------------------------------------------------------------------------
 
 # Second argument should be my command
-command_list = ["new", "backup", "check", "write"]
+command_list = ["new", "backup", "write"]
 command = sys.argv[1]
 
 # Check to see if command is a subset of my commands available
 if not (command in command_list):
 	print "You inputted an invalid command, you fool!"
-	print "Here art what it to be!:\n"
+	print "Here\'s how you do it:\n"
 	print properUse()
 	sys.exit()
+
+# Heck, if the command is most definitely valid, then by golly process the thing
+if command == 'new':
+	new()
+	sys.exit()
 	
+if command == 'backup':
 	
+	# Check for additional optional arguments and fill them with default values
+	# if needed
+	outputFile = os.path.abspath('../../../Dinosaur_RAWR_backup.img')
+	
+	if '-o' in sys.argv:
+		# Find where the -o flag is, and use the argument after that
+		outputFile = os.path.abspath(sys.argv[sys.argv.index('-o') + 1])
+	
+	# Run the method, then exit
+	backup(outputFile)
+	sys.exit()
+
+if command == 'write':
+	
+	# Check for the other arguments
+	outputFile = sys.argv[2]
+	warning = False
+	append = False
+	
+	if '-w' in sys.argv:
+		warning = True
+	
+	if '-a' in sys.argv:
+		append = True
+		
+	# Issue the command and exit
+	write(outputFile, warning, append)
+	sys.exit()
