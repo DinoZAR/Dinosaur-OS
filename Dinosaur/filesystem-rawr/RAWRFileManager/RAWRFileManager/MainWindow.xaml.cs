dd2b7ebe6a0b6b804a8b20f053c4df597214c757
Spencer Graffe <sgraffe@gmail.com>
2012-02-14 12:56:21 -0800

    More work done on RAWR utility. Also used some background threads when necessary.

--- Dinosaur/filesystem-rawr/RAWRFileManager/RAWRFileManager/MainWindow.xaml.cs
+++ Dinosaur/filesystem-rawr/RAWRFileManager/RAWRFileManager/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -14,6 +13,7 @@ using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.IO;
+using System.ComponentModel;
 
 namespace RAWRFileManager
 {
@@ -23,7 +23,7 @@ namespace RAWRFileManager
 	public partial class MainWindow : Window
 	{
 
-		// Private stuff
+		// Progress Windows
 		FormatRAWRProgress pgFormatRAWR;
 
 		public MainWindow()
@@ -54,103 +54,120 @@ namespace RAWRFileManager
 				// Show my progress window of formatting my RAWR filesystem
 				pgFormatRAWR.Show();
 
-				// Start creation of my file
-				BinaryWriter bw = new BinaryWriter(File.Open(dlg.FileName, FileMode.Create));
+				// Create a BackgroundWorker that will create my RAWR file system
+				BackgroundWorker rawrWriter = new BackgroundWorker();
 
-				// Create my RAWR header
-				string rawrLabel = "RAWR File System";
-				byte majorVersion = 0;
-				byte minorVersion = 5;
-				uint lastAccessTime = 4294967295; // just a random number for now, all 1's in binary
-				uint sectorSize = 512; // typical sector size, in bytes
-				uint sizeFileSystem = 102400; // number of sectors, 50 MB
-				uint blockSize = (sizeFileSystem - 1) / 256; // in sectors, allow 256 blocks. Excludes RAWR Header.
-				uint memoryUnitSize = ((blockSize - 1)* sectorSize) / 1024; // in bytes, allowing 1024 memory units in each block
+				rawrWriter.DoWork += new DoWorkEventHandler(rawrWriter_DoWork);
+				rawrWriter.ProgressChanged += new ProgressChangedEventHandler(rawrWriter_ProgressChanged);
+				rawrWriter.RunWorkerCompleted += new RunWorkerCompletedEventHandler(rawrWriter_RunWorkerCompleted);
 
-				// Set my minimum and maximum values here
-				pgFormatRAWR.FormatProgressBar.Minimum = 0;
-				pgFormatRAWR.FormatProgressBar.Maximum = sizeFileSystem;
-				pgFormatRAWR.FormatProgressBar.Value = 0;
+				rawrWriter.WorkerReportsProgress = true;
+				rawrWriter.RunWorkerAsync(dlg.FileName); // Passing in my file name to the worker
+			}
+		}
 
-				// Write values to file
-				bw.Write(Encoding.ASCII.GetBytes(rawrLabel)); // have to use this method or else it writes string length before string, which I don't want
-				bw.Write(majorVersion);
-				bw.Write(minorVersion);
-				bw.Write(lastAccessTime);
-				bw.Write(sectorSize);
-				bw.Write(sizeFileSystem);
-				bw.Write(blockSize);
-				bw.Write(memoryUnitSize);
+		void rawrWriter_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			// Hide my progress window
+			if (pgFormatRAWR.IsActive)
+				pgFormatRAWR.Hide();
 
-				// Create 0's to fill up the sector
-				byte zero = 0;
-				while (bw.BaseStream.Position < sectorSize)
+			// Make my newly created RAWR file my selected file
+
+		}
+
+		void rawrWriter_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			// Update my RAWR progress bar depending on the change
+			pgFormatRAWR.FormatProgressBar.Value = e.ProgressPercentage;
+		}
+
+		void rawrWriter_DoWork(object sender, DoWorkEventArgs e)
+		{
+			// Cast my sender as a BackgroundWorker, since that is what it is
+			BackgroundWorker s = (BackgroundWorker)sender;
+			
+			// Start creation of my file
+			BinaryWriter bw = new BinaryWriter(File.Open((string)e.Argument, FileMode.Create));
+
+			// Create my RAWR header
+			string rawrLabel = "RAWR File System";
+			byte majorVersion = 0;
+			byte minorVersion = 5;
+			uint lastAccessTime = 4294967295; // just a random number for now, all 1's in binary
+			uint sectorSize = 512; // typical sector size, in bytes
+			uint sizeFileSystem = 102400; // number of sectors, 50 MB
+			uint blockSize = (sizeFileSystem - 1) / 256; // in sectors, allow 256 blocks. Excludes RAWR Header.
+			uint memoryUnitSize = ((blockSize - 1) * sectorSize) / 1024; // in bytes, allowing 1024 memory units in each block
+
+			// Write values to file
+			bw.Write(Encoding.ASCII.GetBytes(rawrLabel)); // have to use this method or else it writes string length before string, which I don't want
+			bw.Write(majorVersion);
+			bw.Write(minorVersion);
+			bw.Write(lastAccessTime);
+			bw.Write(sectorSize);
+			bw.Write(sizeFileSystem);
+			bw.Write(blockSize);
+			bw.Write(memoryUnitSize);
+
+			// Create 0's to fill up the sector
+			byte zero = 0;
+			while (bw.BaseStream.Position < sectorSize)
+			{
+				bw.Write(zero);
+			}
+
+
+			// Now define my block header which I will copy everywhere I need to put one
+			string blockLabel = "BLCK";
+			uint memoryUnits = ((blockSize - 1) * sectorSize) / memoryUnitSize;
+			uint memoryUnitsBytes = 0;
+
+			if ((memoryUnits % 8) == 0)
+			{
+				memoryUnitsBytes = memoryUnits / 8;
+			}
+			else
+			{
+				memoryUnitsBytes = (memoryUnits / 8) + 1;
+			}
+
+
+			byte[] memBitmap = new byte[memoryUnitsBytes];
+
+			for (int i = 0; i < memoryUnitsBytes; i++)
+			{
+				memBitmap[i] = 0xFF;
+			}
+
+			// Copy my block header throughout file system
+			uint x = 1; // in sectors
+			while (x < sizeFileSystem)
+			{
+				bw.Write(Encoding.ASCII.GetBytes(blockLabel));
+				bw.Write(memBitmap);
+
+				// Fill rest of sector with 0's
+				for (int i = 0; i < (sectorSize - memBitmap.Length - 4); i++)
 				{
 					bw.Write(zero);
 				}
 
-
-				// Now define my block header which I will copy everywhere I need to put one
-				string blockLabel = "BLCK";
-				uint memoryUnits = ((blockSize - 1) * sectorSize) / memoryUnitSize;
-				uint memoryUnitsBytes = 0;
-
-				if ((memoryUnits % 8) == 0)
+				// Fill rest of block partition with 0's
+				for (int i = 0; i < ((blockSize - 1) * sectorSize); i++)
 				{
-					memoryUnitsBytes = memoryUnits / 8;
-				}
-				else
-				{
-					memoryUnitsBytes = (memoryUnits / 8) + 1;
+					bw.Write(zero);
 				}
 
+				x += blockSize;
 
-				byte[] memBitmap = new byte[memoryUnitsBytes];
+				double percentage = ((double)x / (double)sizeFileSystem) * 100;
 
-				for (int i = 0; i < memoryUnitsBytes; i++)
-				{
-					memBitmap[i] = 0xFF;
-				}
-
-
-				// Update text saying that I am applying blocks throughout file system
-				pgFormatRAWR.FormatLabel.Content = "Writing block headers...";
-
-				// Copy my block header throughout file system
-				uint x = 1; // in sectors
-				while (x < sizeFileSystem)
-				{
-					bw.Write(Encoding.ASCII.GetBytes(blockLabel));
-					bw.Write(memBitmap);
-
-					// Fill rest of sector with 0's
-					for (int i = 0; i < (sectorSize - memBitmap.Length - 4); i++)
-					{
-						bw.Write(zero);
-					}
-
-					// Fill rest of block partition with 0's
-					for (int i = 0; i < ((blockSize - 1) * sectorSize); i++)
-					{
-						bw.Write(zero);
-					}
-
-					Debug.Print("Finished block");
-					Debug.Print(x.ToString());
-					Debug.Print(sizeFileSystem.ToString());
-
-					x += blockSize;
-
-					pgFormatRAWR.FormatProgressBar.Value = x;
-				}
-
-				// After everything has been done, close the file
-				Debug.Print("Done!");
-				bw.Close();
-
-				// Close the progress thingy
-				pgFormatRAWR.Close();
+				s.ReportProgress((int)percentage);
 			}
+
+			// After everything has been done, close the file
+			bw.Close();
 		}
 
 		private void Window_Closed(object sender, EventArgs e)
@@ -159,29 +176,5 @@ namespace RAWRFileManager
 		}
 	}
 	
-	//
-	// All of my possible memory units and the classes to represent them
-	//
-	public class DirectoryUnit
-	{
-		public string Name { get; set; }
-		public CompositeCollection Children { get; set; }
-
-		public DirectoryUnit(string name)
-		{
-			Name = name;
-		}
-	}
-
-	public class FileUnit
-	{
-		public string Name { get; set; }
-		public int Size { get; set; }
-
-		public FileUnit(string name, int size)
-		{
-			Name = name;
-			Size = size;
-		}
-	}
+	
 }
---
+++ Dinosaur/filesystem-rawr/RAWRFileManager/RAWRFileManager/RAWRManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace RAWRFileManager
+{
+	/// <summary>
+	/// The RAWRManager is responsible for retrieving data from the virtual RAWR disc it is assigned to. The UI will use its functions to obtain data from it.
+	/// </summary>
+	public class RAWRManager
+	{
+		public string RAWRFile { get; set; }
+
+		public RAWRManager(string rawrFile)
+		{
+			RAWRFile = rawrFile;
+		}
+
+
+		public MemoryUnit Get(string absPath)
+		{
+			return null;
+		}
+
+		public MemoryUnit[] ReadDirectory(DirectoryUnit directory)
+		{
+			return null;
+		}
+	}
+
+
+	/*
+	 * Classes that encapsulate data retrieved from RAWR memory units
+	 */
+	public enum MemUnitType {File, Continuation, Directory, Link};
+
+	public class MemoryUnit
+	{
+		public MemUnitType Type {get;}
+		public string Name { get; set; }
+
+	}
+
+	public class DirectoryUnit : MemoryUnit
+	{
+		public CompositeCollection Children { get; set; }
+
+		public DirectoryUnit(string name)
+		{
+			Name = name;
+		}
+	}
+
+	public class FileUnit : MemoryUnit
+	{
+		public int Size { get; set; }
+
+		public FileUnit(string name, int size)
+		{
+			Name = name;
+			Size = size;
+		}
+	}
+
+	public class LinkUnit : MemoryUnit
+	{
+		public uint Block { get; set; }
+		public uint MemUnitAddress { get; set; }
+
+		public LinkUnit(uint blockAddress, uint memUnitAddress)
+		{
+			Block = blockAddress;
+			MemUnitAddress = memUnitAddress;
+		}
+
+	}
+}
